858b440fcf0e5ead7e1a1534ef1768b193b6ab90
Walt Seymour <waltonseymour@users.noreply.github.com>
2020-03-30 10:31:51 -0400

    iterator impl (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
-use std::io::Write;
+pub struct PrimeIterator {
+    current: usize,
+    data: std::vec::Vec<bool>,
+}
 
-fn primes(n: usize) {
-    let mut stdout = std::io::stdout();
-    let mut data = vec![false; n];
+fn new_prime_iterator(size: usize) -> PrimeIterator {
+    PrimeIterator {
+        current: 2,
+        data: vec![false; size],
+    }
+}
 
-    for i in 2..n {
-        if data[i] == false {
-            writeln!(stdout, "{prime}", prime = i);
-            let mut j = i;
-            while j < n {
-                data[j] = true;
-                j += i;
+impl std::iter::Iterator for PrimeIterator {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.current < self.data.len() {
+            if self.data[self.current] == false {
+                let mut j = self.current;
+
+                while j < self.data.len() {
+                    self.data[j] = true;
+                    j += self.current;
+                }
+                return Some(self.current);
             }
+            self.current += 1;
         }
+        None
     }
 }
 
 fn main() {
-    primes(100)
+    let iter = new_prime_iterator(10000);
+
+    for prime in iter {
+        println!("{p}", p = prime);
+    }
 }
