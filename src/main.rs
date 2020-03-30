03e10e8f4585ec430c68a683182dd58a87f7ebd4
waltonseymour <walton.l.seymour@gmail.com>
2020-03-30 10:05:50 -0400

    scaffold for io::Read impl

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-use std::io::Write;
+pub struct PrimeReader {
+    current: usize,
+    data: std::vec::Vec<bool>,
+}
 
-fn primes(n: usize) {
-    let mut stdout = std::io::stdout();
-    let mut data = vec![false; n];
-
-    for i in 2..n {
-        if data[i] == false {
-            writeln!(stdout, "{prime}", prime = i);
-            let mut j = i;
-            while j < n {
-                data[j] = true;
-                j += i;
+impl std::io::Read for PrimeReader {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        while self.current < self.data.len() {
+            if self.data[self.current] == false {
+                //buf.
             }
         }
+
+        Ok(2)
+    }
+
+    fn read_vectored(&mut self, bufs: &mut [std::io::IoSliceMut<'_>]) -> std::io::Result<usize> {
+        Ok(2)
     }
 }
 
 fn main() {
-    primes(100)
+    // primes(100)
 }
